110d6c496e2d1a6421f4b6be904c7e6c714ed46d
Akashkumar4 <akash.kumar@oodlestechnologies.com>
2020-04-19 15:51:48 +0530

    first project

---
+++ src/main/java/com/akash/mysqlwithboot/DTO/RequestDTO.java
@@ -0,0 +1,4 @@
+package com.akash.mysqlwithboot.DTO;
+
+public class RequestDTO {
+}
---
+++ src/main/java/com/akash/mysqlwithboot/MysqlWithBootApplication.java
@@ -0,0 +1,13 @@
+package com.akash.mysqlwithboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlWithBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlWithBootApplication.class, args);
+	}
+
+}
